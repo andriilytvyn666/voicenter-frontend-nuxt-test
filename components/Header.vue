c472b9083a1f6698ad4ed7731bf9299313cd190e
Andrii Lytvyn <lytvyn349@gmail.com>
2023-10-05 01:08:59 +0300

    refactor: swap NuxtLink and h3

--- components/Header.vue
+++ components/Header.vue
@@ -1,16 +1,16 @@
 <template>
-  <nav
+  <header
     class="px-[4.625rem] flex justify-between items-center bg-[#D9D9D9] h-[6.313rem]"
     id="nav"
   >
     <!-- <NuxtImg alt="LOGO" /> -->
     <h1 class="font-medium text-[25px]">LOGO</h1>
-    <div class="flex gap-8 font-bold">
-      <h3><NuxtLink class="nav-link" href="#item1">NAV 1</NuxtLink></h3>
-      <h3><NuxtLink class="nav-link" href="#item2">NAV 2</NuxtLink></h3>
-      <h3><NuxtLink class="nav-link" href="#item3">NAV 3</NuxtLink></h3>
-    </div>
-  </nav>
+    <nav class="flex gap-8 font-bold">
+      <NuxtLink class="nav-link" href="#item1"><h3>NAV 1</h3></NuxtLink>
+      <NuxtLink class="nav-link" href="#item2"><h3>NAV 2</h3></NuxtLink>
+      <NuxtLink class="nav-link" href="#item3"><h3>NAV 3</h3></NuxtLink>
+    </nav>
+  </header>
 </template>
 
 <style lang="postcss">
--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,14 +1,14 @@
 <template>
-  <div
+  <aside
     class="pl-[4.5rem] pr-[5rem] max-w-[17.25rem] min-w-[17.25rem] min-h-full"
   >
-    <nav class="font-bold flex flex-col gap-10 pt-[6.5rem]">
+    <nav class="font-bold flex flex-col gap-10 pt-[6.5rem] sticky top-0">
       <h3><NuxtLink class="nav-link" href="#nav">NAV</NuxtLink></h3>
       <div class="flex flex-col gap-3">
-        <h3><NuxtLink class="nav-link" href="#item1">ITEM 1</NuxtLink></h3>
-        <h3><NuxtLink class="nav-link" href="#item2">ITEM 2</NuxtLink></h3>
-        <h3><NuxtLink class="nav-link" href="#item3">ITEM 3</NuxtLink></h3>
+        <NuxtLink class="nav-link" href="#item1"><h3>ITEM 1</h3></NuxtLink>
+        <NuxtLink class="nav-link" href="#item2"><h3>ITEM 2</h3></NuxtLink>
+        <NuxtLink class="nav-link" href="#item3"><h3>ITEM 3</h3></NuxtLink>
       </div>
     </nav>
-  </div>
+  </aside>
 </template>
